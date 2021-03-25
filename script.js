d9b9a17af23a1d65e28ec0462fb717cbae22e668
Serena Chandler <serenachandler2019@gmail.com>
2021-03-25 09:27:35 -0700

    added the new funtions into the main one, assigned them variables

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var letter = ["a", "b", "c", "d"]
-var special = ["!", "@", "$", "%", "`", "&", "$", "*"]
-var num = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
-var final = []
+var letterArray = ["a", "b", "c", "d"]
+var specialArray = ["!", "@", "$", "%", "`", "&", "$", "*"]
+var numberArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+var finalArray = []
 
 
 function letters() {
-  var letter = prompt("Would you like your password to contain letters?") 
-  return letter   
+  var wantletter = prompt("Would you like your password to contain letters?") 
+  return wantletter   
 }
 
 function specials() {
-  var special = prompt("would you like your password to contain special characters?")
-  return special
+  var wantSpecial = prompt("would you like your password to contain special characters?")
+  return wantSpecial
 }
 
 function numbers() {
-  var number = prompt("would you like your password to contain numbers?")
-  return number
+  var wantNumber = prompt("would you like your password to contain numbers?")
+  return wantNumber
 }
 
 function Uppers() {
-  var upper = prompt("would you like your password to contain uppercase letters?")
-  return upper
+  var wantUpper = prompt("would you like your password to contain uppercase letters?")
+  return wantUpper
 }
 
 
@@ -39,27 +39,28 @@ function writePassword() {
     length = 0
     while (length < 8 || length >128) {
     var length = prompt("how long do you want your password to be? Please select bewteen 8 and 120 characters")
-    if (length < 8 || length > 128 || length == String) {
+    if (length < 8 || length > 128 ) {
       window.alert("please enter a number between 8 and 120")
     }
     }
 
-    letters()
-    specials()
-    numbers()
-    Uppers()
+    
+    var special = specials()
+    var number = numbers()
+    var letter = letters()
+    var upper = Uppers()
+    
 
 
+    if (letter == "yes" && special == "yes") {
+      for (i = 0; i < length/3 ; i++) {
+        finalArray.unshift(letterArray[Math.floor(Math.random()*4)])
+        // finalArray.unshift(specialArray[Math.floor(Math.random()*4)])
+        // finalArray.unshift(numberArray[Math.floor(Math.random()*4)])
+      }}
+    
 
-    if (upper == "yes")
-    for (i = 0; i < length; i++) {
-      final.unshift(letter[Math.floor(Math.random()*4)])
-      final.unshift(special[Math.floor(Math.random()*4)])
-      final.unshift(num[Math.floor(Math.random()*4)])
-    }
-
-
-  return final
+  return finalArray
   }
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
